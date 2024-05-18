0752511619eddc6262b53d905430d09ca0e3429d
dtclxy64 <70486866+dtclxy64@users.noreply.github.com>
2024-05-18 18:35:59 -0400

    Update local-up.sh
    reduce the number of kind clusters due to inotify limits

--- solutions/setup-dev-environment/local-up.sh
+++ solutions/setup-dev-environment/local-up.sh
@@ -6,16 +6,17 @@ set -e
 
 hub=${CLUSTER1:-hub}
 c1=${CLUSTER1:-cluster1}
-c2=${CLUSTER2:-cluster2}
+# only create 2 clusters due to inotify limit
+#c2=${CLUSTER2:-cluster2}
 
 hubctx="kind-${hub}"
 c1ctx="kind-${c1}"
-c2ctx="kind-${c2}"
+#c2ctx="kind-${c2}"
 
 # using v1.22.17 Kind node images for compatibility with KubeVela
 kind create cluster --name "${hub}" --image kindest/node:v1.22.17
 kind create cluster --name "${c1}" --image kindest/node:v1.22.17
-kind create cluster --name "${c2}" --image kindest/node:v1.22.17
+#kind create cluster --name "${c2}" --image kindest/node:v1.22.17
 
 echo "Initialize the ocm hub cluster\n"
 clusteradm init --wait --context ${hubctx}
@@ -24,10 +25,13 @@ joincmd=$(clusteradm get token --context ${hubctx} | grep clusteradm)
 echo "Join cluster1 to hub\n"
 $(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c1ctx} | sed "s/<cluster_name>/$c1/g")
 
-echo "Join cluster2 to hub\n"
-$(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c2ctx} | sed "s/<cluster_name>/$c2/g")
+#echo "Join cluster2 to hub\n"
+#$(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c2ctx} | sed "s/<cluster_name>/$c2/g")
 
-echo "Accept join of cluster1 and cluster2"
-clusteradm accept --context ${hubctx} --clusters ${c1},${c2} --wait
+#echo "Accept join of cluster1 and cluster2"
+#clusteradm accept --context ${hubctx} --clusters ${c1},${c2} --wait
+
+echo "Accept join of cluster1"
+clusteradm accept --context ${hubctx} --clusters ${c1} --wait
 
 kubectl get managedclusters --all-namespaces --context ${hubctx}
