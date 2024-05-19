77fd868d36e23d78e68e67379e4202149d0b0d7d
dtclxy64 <70486866+dtclxy64@users.noreply.github.com>
2024-05-18 20:54:11 -0400

    Update local-up.sh

--- solutions/setup-dev-environment/local-up.sh
+++ solutions/setup-dev-environment/local-up.sh
@@ -6,18 +6,17 @@ set -e
 
 hub=${CLUSTER1:-hub}
 c1=${CLUSTER1:-cluster1}
-# only create 2 clusters due to inotify limit
-#c2=${CLUSTER2:-cluster2}
+c2=${CLUSTER2:-cluster2}
 
 hubctx="kind-${hub}"
 c1ctx="kind-${c1}"
-#c2ctx="kind-${c2}"
+c2ctx="kind-${c2}"
 
 # using v1.22.17 Kind node images for compatibility with KubeVela
 # separate the kubeconfigs to have KubeVela manage the clusters
 kind create cluster --name "${hub}" --image kindest/node:v1.22.17 --kubeconfig ~/.kube/hub.kubeconfig
 kind create cluster --name "${c1}" --image kindest/node:v1.22.17 --kubeconfig ~/.kube/cluster1.kubeconfig
-#kind create cluster --name "${c2}" --image kindest/node:v1.22.17
+kind create cluster --name "${c2}" --image kindest/node:v1.22.17 --kubeconfig ~/.kube/cluster2.kubeconfig
 
 echo "Initialize the ocm hub cluster\n"
 export KUBECONFIG=~/.kube/hub.kubeconfig
@@ -28,11 +27,12 @@ echo "Join cluster1 to hub\n"
 export KUBECONFIG=~/.kube/cluster1.kubeconfig
 $(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c1ctx} | sed "s/<cluster_name>/$c1/g")
 
-#echo "Join cluster2 to hub\n"
-#$(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c2ctx} | sed "s/<cluster_name>/$c2/g")
+echo "Join cluster2 to hub\n"
+export KUBECONFIG=~/.kube/cluster2.kubeconfig
+$(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c2ctx} | sed "s/<cluster_name>/$c2/g")
 
-#echo "Accept join of cluster1 and cluster2"
-#clusteradm accept --context ${hubctx} --clusters ${c1},${c2} --wait
+echo "Accept join of cluster1 and cluster2"
+clusteradm accept --context ${hubctx} --clusters ${c1},${c2} --wait
 
 echo "Accept join of cluster1"
 export KUBECONFIG=~/.kube/hub.kubeconfig
